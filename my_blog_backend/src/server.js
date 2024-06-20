9959c67c427dd7807b56015102372161d847bf78
T2bex <tobechukwu14@yahoo.com>
2024-06-20 23:05:12 +0100

    mongo creation

--- my_blog_backend/src/server.js
+++ my_blog_backend/src/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { MongoClient } from 'mongodb';
 
 const app = express();
 app.use(express.json())
@@ -11,26 +12,41 @@ app.use(express.json())
 //     const{name} = req.params
 //     res.send(`Hello ${name}!!`)
 // })
-let articleInfo = [{
-    name: 'learn-react',
-    upvotes: 0,
-    comments: [],
-},{
-    name: 'learn-node',
-    upvotes: 0,
-    comments: [],
-},{
-    name: 'mongodb',
-    upvotes: 0,
-    comments: [],
-},
-]
-
-app.put('/api/articles/:name/upvote', (req,res) => {
+app.get('/api/articles/:name', async (req,res) => {
     const {name} = req.params
-    const article = articleInfo.find(a => a.name === name)
+
+    const client = new MongoClient('mongodb://127.0.0.1:27017')
+
+    await client.connect()
+
+    const db = client.db('react_blog_db') //use react_blog_db
+
+    const article = await db.collection('articles').findOne({ name })
+
     if (article){
-        article.upvotes += 1
+        res.json(article)
+    }else {
+        res.sendStatus(404)
+    }
+
+    
+})
+
+app.put('/api/articles/:name/upvote', async (req,res) => {
+    const {name} = req.params
+    
+    const client = new MongoClient('mongodb://127.0.0.1:27017')
+
+    await client.connect()
+
+    const db = client.db('react_blog_db') //use react_blog_db
+    await db.collection('articles').updateOne({name}, {
+        $inc: {upvotes: 1},
+    })
+
+    const article = await db.collection('articles').findOne({ name })
+    
+    if (article){
         res.send(`The ${name} article now has ${article.upvotes} upvotes`)
     } else{
         res.send('That article doesn\'t exist')
@@ -40,10 +56,9 @@ app.put('/api/articles/:name/upvote', (req,res) => {
 app.post('/api/articles/:name/comments', (req, res) => {
     const {name} = req.params
     const {postedBy, text} = req.body
-    const article = articleInfo.find(a => a.name === name)
+    
 
     if (article) {
-        article.comments.push({postedBy, text})
         res.send(article.comments)
     } else{
         res.send('That article doesn\'t exist')
